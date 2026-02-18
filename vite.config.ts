2729f09b7a4a8069250cc69299bd065e87ca44e5
まろん <kaki123xxxkaki456@gmail.com>
2026-02-18 22:42:12 +0900

    Copy CMap and font files to build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/pdfjs-dist/cmaps/*',
+          dest: 'cmaps'
+        },
+        {
+          src: 'node_modules/pdfjs-dist/standard_fonts/*',
+          dest: 'standard_fonts'
+        }
+      ]
+    })
+  ],
   build: {
     target: 'esnext'
   },
